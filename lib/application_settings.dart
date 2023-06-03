e86650b1d2536c3d0392a45679f0fa0dd1016db0
Matheus de Sousa Barros <bmatheus91@gmail.com>
2023-06-03 18:56:07 -0300

    Adicao do Bloc

---
+++ lib/application_settings.dart
@@ -0,0 +1,52 @@
+import 'package:bloc/bloc.dart';
+
+import 'package:flutter_hospital_dashboard/routes.dart';
+
+class ApplicationSettings extends Bloc<SettingsEvent, SettingsState> {
+  String _currentRoute = ApplicationRoutes.home;
+
+  ApplicationSettings() : super(IdleApplication()) {
+    on<StartApplication>(_onStartApplication);
+    on<UpdateCurrentRoute>(setRoute);
+  }
+
+  String get currentRoute => _currentRoute;
+
+  void _onStartApplication(
+    StartApplication event,
+    Emitter<SettingsState> emit,
+  ) async {
+    emit(InitializingApplication());
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    emit(LoadedApplication());
+  }
+
+  void setRoute(
+    UpdateCurrentRoute event,
+    Emitter<SettingsState> emit,
+  ) {
+    _currentRoute = event.route;
+  }
+
+  bool isRouteSelected(String route) => _currentRoute.startsWith(route);
+}
+
+abstract class SettingsEvent {}
+
+class StartApplication extends SettingsEvent {}
+
+class UpdateCurrentRoute extends SettingsEvent {
+  UpdateCurrentRoute(this.route);
+
+  final String route;
+}
+
+abstract class SettingsState {}
+
+class IdleApplication extends SettingsState {}
+
+class InitializingApplication extends SettingsState {}
+
+class LoadedApplication extends SettingsState {}
